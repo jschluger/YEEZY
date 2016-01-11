3db210544e52e3a5f92071491406466cfded84cc
jschluger <jschluger@stuy.edu>
2016-01-11 09:54:46 -0500

    init commit

---
+++ T048.java
@@ -0,0 +1,14 @@
+/*====================================                                     
+  class 2048 --  implements the game 2048
+
+  implements the game interface
+  ====================================*/
+
+public class T048 implements Game {
+    
+    //instance variables
+    
+    //
+    private Board _board = new Board(4);
+    
+}
