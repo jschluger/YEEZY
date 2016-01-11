a54b94db138e935054abea5d0cca3a5b4415cefd
Jack Schluger <jschluger@stuy.edu>
2016-01-11 17:26:59 -0500

    added more methods from Matrix.java

--- Board.java
+++ Board.java
@@ -35,6 +35,70 @@ public class Board {
 	}
     }
 
+    //return the item at the specified row & column
+    // O(1)
+    private Object get( int r, int c ) {
+	return _board[r][c];
+    } // O(1)   
+
+      //overwrite item at specified row and column with newVal
+    //return old value
+    // O(1)
+    private Object set( int r, int c, Object newVal ) {
+	Object oldVal = get(r,c); // save oldVal
+	_board[r][c] = newVal;
+	return oldVal;
+    } // O(1)
+
+    public Object[] getRow( int r ) {
+	return _board[r];
+    }
+
+    public Object[] setRow( int r, Object[] newRow ) {
+	Object[] temp = getRow(r); // save old row
+	_board[r] = newRow;
+	return temp;
+    } // O(1)
+
+    public Object[] getCol( int c ) {
+	Object[] retVal = new Object[size()];
+	for (int r = 0; r < size(); r++){
+	    retVal[r] = _board[r][c];
+	}
+	return retVal;
+    } // O(n)
+
+    public Object[] setCol( int c, Object[] newCol ) {
+	Object[] temp = getCol(c); // save old col
+	for (int r = 0; r < size(); r++){
+	    _board[r][c] = newCol[r];
+	}
+	return temp;
+    } // O(n)
+
+    
+    public void transpose() {
+	Board temp = new Board(size());
+	// copy _board to temp to avoid aliasing
+	for (int i = 0; i < size(); i++) {
+	    for (int j = 0; j < size(); j++) {
+		temp.set(i, j, get(i,j));
+	    }
+	}
+
+	for (int i = 0; i < size(); i++) {
+	    setRow(i,temp.getCol(i)); // transpose
+	}
+    } // O(n^2)
+    
+    public boolean contains( Object o ) {
+	for (int r = 0; r < size(); r++)
+	    for (int c = 0; c < size(); c++)
+		if ( o.equals(get(r,c)) ) return true; // true if equivalence found
+	return false;
+    } // O(n^2)
+
+    
     // override toString()
     public String toString() {
 	String retStr = "";
