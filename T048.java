9f1f39c8ab6290118c83ec24a90b28906903e6dd
Jack Schluger <jschluger@stuy.edu>
2016-01-11 21:18:14 -0500

    added some testing

--- T048.java
+++ T048.java
@@ -289,6 +289,22 @@ public class T048 {
       ====================================*/
     public static void main(String[] args){
 	play();
+	/*
+	T048 t = new T048();
+	t._board.set(3,0,1024);
+	t._board.set(3,1,512);
+	t._board.set(3,2,256);
+	t._board.set(3,3,128);
+	t._board.set(2,3,64);
+	t._board.set(2,2,32);
+	t._board.set(2,1,16);
+	t._board.set(2,0,8);
+	t._board.set(1,0,4);
+	t._board.set(1,1,2);
+	t._board.set(1,2,2);
+	t.playGame();*/
+
+
     }//end main
     
 }//end class T048
