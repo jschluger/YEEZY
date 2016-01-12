479240ef8ae11fe4c4cfbf9f4a080dfe1b566af2
Manish Saha <msaha@stuy.edu>
2016-01-11 23:11:26 -0500

    final standardization

--- Battleship.java
+++ Battleship.java
@@ -2,6 +2,7 @@
   class Battleship - the classic Battleship game 
 
   implements Game interface
+  uses Board as data type
   ====================================*/
 
 import cs1.Keyboard;
@@ -9,20 +10,21 @@ import cs1.Keyboard;
 public class Battleship implements Game {
 
     private Board _board;
-    private int _shipRow;
-    private int _shipCol;
+    private int _shipRow; // location of
+    private int _shipCol; // battleship
     private int _size;
-    private int _numTurns;
+    private int _numTurns; // algo: (1/4) * area of ocean
 
+    // guessed values
     private int _row;
     private int _col;
 
     // default constructor with default board
     public Battleship() {
 	_board = new Board();
-	_board.populate("O"); // denotes unknown point in sea
+	_board.populate("O"); // denotes unknown point in ocean
         _size = _board.size();
-	_numTurns = _size;
+	_numTurns = (_size * _size)/4; 
 	_shipRow = (int)(Math.random() * _size) + 1; // [1,size]
 	_shipCol = (int)(Math.random() * _size) + 1; // [1,size]
     }
@@ -30,9 +32,9 @@ public class Battleship implements Game {
     // overloaded constructor with customized board
     public Battleship(int a) {
 	_board = new Board(a);
-	_board.populate("O"); // denotes unknown point in sea
+	_board.populate("O"); // denotes unknown point in ocean
         _size = a;
-	_numTurns = _size;
+	_numTurns = (a * a)/4; 
 	_shipRow = (int)(Math.random() * a) + 1; // [1,a]
 	_shipCol = (int)(Math.random() * a) + 1; // [1,a]
     }
@@ -65,21 +67,35 @@ public class Battleship implements Game {
 	return _board.contains("H");
     }
 
-    public void playGame() {
-	while (_numTurns > 0) {
-	    System.out.print(_board);
-	    System.out.println("TURNS LEFT: " + _numTurns + "\n");
+    // for UX
+    public void displayInstructions() {
+        System.out.println("Welcome to BATTLESHIP, where you will try to sink my sole battleship in this vast ocean! Be strategic and make wise decisions, or else fail miserably!\n");
+	System.out.println("Your goal is to sink my battleship. Enter integer values for the row and column you think my battleship is located in. You only have a set amount of turns, so be careful!\n");
+	System.out.println("Try not to go out of boundaries or choose the same location!\n\nX: Marks missed spot\n\nH: Battleship location\n");
+    }
+
+    // simulates one turn of Battleship
+    public void playTurn() {
+	System.out.print(_board);
+	System.out.println("TURNS LEFT: " + _numTurns + "\n");
 	    
-	    // user input for row and column using Keyboard class
-	    System.out.print("Row: ");
-	    _row = Keyboard.readInt();
-	    System.out.print("Column: ");
-	    _col = Keyboard.readInt();
+	// user input for row and column using Keyboard class
+	System.out.print("Row: ");
+	_row = Keyboard.readInt();
+	System.out.print("Column: ");
+	_col = Keyboard.readInt();
 
-	    System.out.println();
+	System.out.println();
 
-	    check(); // verify values
-	    _numTurns -= 1; // decimate turns left by 1
+	check(); // verify values
+	_numTurns -= 1; // decimate turns left by 1
+    }
+
+    // play a game of Battleship in a nonstatic context
+    public void playGame() {
+	displayInstructions();
+	while (_numTurns > 0) {
+	    playTurn();
 
 	    // check if game has been completed before user has exhausted the number of turns available
 	    if (isGameOver()) {
@@ -90,21 +106,33 @@ public class Battleship implements Game {
 	
 	// check to see if user has failed to correctly pinpoint battleship
 	if (!(isGameOver())) {
-	    System.out.println("Despite your great efforts, you have failed to sink my battleship in this vast sea. Success awaits.\n");
+	    System.out.println("Despite your great efforts, you have failed to sink my battleship in this vast ocean. Success awaits.\n");
 	    _board.set(_shipRow - 1, _shipCol - 1, "H"); // show true location
 	    System.out.print(_board);
 	}
     }
 
+    // plays an entire game of Battleship from static context
     public static void play() {
-	System.out.println("Welcome to BATTLESHIP, where you will try to sink my sole battleship in this vast sea! Be strategic and make wise decisions, or else fail miserably!\n");
-
-	System.out.println("INSTRUCTIONS\nYour goal is to sink my battleship. Enter integer values for the row and column you think my battleship is located in. You only have a set amount of turns, so be careful!\n");
-
-	Battleship game = new Battleship();
+	// allow for user input of size of ocean
+	boolean passed = false;
+	int s = 5;
+	while (!passed) { // continue to try until size within specified boundaries
+	    System.out.print("Please enter the width of the ocean within 5 - 10 units, inclusive: ");
+	    s = Keyboard.readInt();
+	    if (s >= 5 && s <= 10) {
+		passed = true;
+	    }
+	    else
+		System.out.println("Try again... ");
+	    System.out.println();
+	}
+	    
+	Battleship game = new Battleship(s);
 	game.playGame();
     }
 
+    // for testing
     public static void main(String[] args) {
 	play();
     }
