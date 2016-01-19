44aaaa403d4d90a68ba96f928c78a337432e8916
Jack Schluger <jschluger@stuy.edu>
2016-01-19 17:01:10 -0500

    inital commit

---
+++ P1.java
@@ -0,0 +1,26 @@
+/*=====================================                                     
+  class P1 -- the first period of the day
+  
+  ====================================*/
+
+import cs1.Keyboard;
+import java.util.ArrayList;
+
+public class P1 extends Period {
+    
+    
+    
+    //wrapper funciton for all that will happen during each period
+    public void scenario(){
+	System.out.println("As you run up the stairs to the fifth floor, you hear the bell ring. You burst into Spanish class and hear Senor Simon yell, 'Por que estas tarde chico? Aye, donde era?'");
+	System.out.println("Your response: ");
+	String s1 = "The 7 train was delayed again!";
+	String s2 = "I overslept";
+    
+
+    }
+    
+    
+    
+    
+} //end class Period
---
+++ Period.java
@@ -0,0 +1,19 @@
+/*=====================================                                     
+  class period -- abstract class for each period of the day 
+  
+  ====================================*/
+
+import cs1.Keyboard;
+import java.util.ArrayList;
+
+public abstract class Period {
+    
+    
+    
+    //wrapper funciton for all that will happen during each period
+    public void scenario();
+    
+    
+    
+    
+} //end class Period
