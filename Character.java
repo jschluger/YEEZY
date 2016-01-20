eb278d8a7ab48b4f198ef3e86ff65bb6a0333f9e
Jack Schluger <jschluger@stuy.edu>
2016-01-19 21:19:32 -0500

    innital commit

---
+++ Character.java
@@ -0,0 +1,14 @@
+/*====================================                                     
+  class Character 
+  
+  ====================================*/
+
+import cs1.Keyboard;
+import java.util.ArrayList;
+
+public class Character {
+    
+    
+    
+
+} //end class Character
