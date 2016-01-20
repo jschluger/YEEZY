36e6ee2fe70842a42ebb15325afb55052fbbb0a0
Manish Saha <msaha@stuy.edu>
2016-01-19 21:53:31 -0500

    added HonStudent class

---
+++ HonStudent.java
@@ -0,0 +1,19 @@
+/*====================================                                                  
+  class HonStudent extends RegStudent                                                                  
+  ====================================*/
+
+import cs1.Keyboard;
+import java.util.ArrayList;
+
+
+public class HonStudent extends RegStudent {
+    
+    public HonStudent() {
+	super();
+	//_schedule.add( new P2() ); didnt write class yet
+	
+    }
+    
+    
+
+}//end class HonStudent
