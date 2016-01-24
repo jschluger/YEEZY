b7f41b7dbbd904cac02bef337e12b106a2240cf6
Jack Schluger <jschluger@stuy.edu>
2016-01-24 18:34:47 -0500

    Final Commit?

--- Character.java
+++ Character.java
@@ -43,14 +43,22 @@ public abstract class Character {
     
     //run after all Periods are run, to see if you won or lost the game
     public void endGame() {
+	System.out.println("The final bell rings to end your last day of high school. You reminisce over the frendships you created, and look forward at your bright future...");
+	Period.pause();
+
+	System.out.println("Later that night, at the graduation ceremony: ");
+	System.out.println("Ms. Zhang: I am proud to announce DJ Khaled as this years guest speaker!");
+
+	
+	System.out.print("\nDJ Khaled: ");
 	if ( getCreditsEarned() == getCreditsNeeded() ) {
-	    System.out.println("Congratulations! You have earned enough credits to graduate Stuyvesant High School. Godspeed.");
+	    System.out.println("Congratulations! You have earned enough credits to graduate Stuyvesant High School! We the best. Join me on the path to success. Lionnn.");
 	}
 	else if ( getCreditsEarned() > getCreditsNeeded() ) {
 	    System.out.println("You a genius. But you have passed with flying colors. Here is your key to success! Another one.");
 	}
 	else {
-	    System.out.println("They don't want you to succeed. You don't graduate :( Another one.");
+	    System.out.println("They don't want you to succeed. You don't graduate Stuyvesant High School. :( Don't let them put you down. Another one.");
 	}
 	
     }
